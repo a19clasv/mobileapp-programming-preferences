ceef7afa2c882db9e925a6dfa03444f2be09a8cf
Claes Svensson <a19clasv@student.his.se>
2022-05-05 13:44:53 +0200

    Saves text in editText to Shared Preferences when button is clicked that then changes textViewName's text immediately after and in onResume

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,11 +1,19 @@
 package com.example.project;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textViewName;
+    SharedPreferences preferences;
+    SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +21,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        textViewName = findViewById(R.id.name);
+
+        preferences = getSharedPreferences("preferences", MODE_PRIVATE);
+        editor = preferences.edit();
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText editText = findViewById(R.id.edit_text);
+                String editedText = editText.getText().toString();
+                editor.putString("name", editedText);
+                editor.apply();
+                textViewName.setText(editedText);
+            }
+        });
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        String name = preferences.getString("name", "inget namn hittades");
+        textViewName.setText(name);
+    }
 }
